54df553046807a848945e4b15a5c87b9e292a006
Tyler Hoover <tjh007@lvc.edu>
2023-03-09 11:35:35 -0500

    Fixed issue with cmake including two versions of the same test

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
+
+// Is this what will happen?
 #include "grader.hpp"
 
 using namespace std;
 
 int main(int, char**) {
     std::cout << "Hello, beetcode world!\n";
-
-    pythonGrader g = pythonGrader();
-
-    // Test a correct
-    bool correct = g.grade_code("x = input()\nprint(x)", "Hello", "Hello");
-    if(correct) {
-        cout << "It worked!" << endl;
-    }
-    else cout << "It didn't work" << endl;
 }
--- test/grader_tests.cpp
+++ test/grader_tests.cpp
@@ -3,12 +3,14 @@
 #include "grader.hpp"
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(GraderTest, CorrectGrade) {
 
     pythonGrader g;
-    bool t = g.grade_code("print(\"Hello\")", "", "Hello");
+    bool t = g.grade_code("x = input()\nprint(x)", "Hello", "Hello");
+    bool ts = g.grade_code("print(\"Hello\")", "", "Hello");
     bool f = g.grade_code("print(\"wrong\")", "", "Right"); 
 
-    EXPECT_EQ(f, false); 
+    EXPECT_EQ(ts, true);
     EXPECT_EQ(t, true);
+    EXPECT_EQ(f, false); 
 }
