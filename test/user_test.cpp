07ace6a1f49258ed23fe17cc0537c329797a6a4b
Brandon Bauer <btb002@lvc.edu>
2023-03-16 22:06:55 -0400

    User is swapped and kinda tested

--- test/user_test.cpp
+++ test/user_test.cpp
@@ -1,22 +1,22 @@
 #include <gtest/gtest.h>
 #include <string>
 
-#include "user.hpp"
+#include "../src/entities/user/user.hpp"
 
 using namespace std;
 
 bool creating_user(){
     string username {"test_user"};
-    PermissionLevel {0};
-    
-
+    PermissionLevel permissions {Admin};
+    User user1(permissions,username);
+    if(user1.get_username()!=username) return false;
+    if(user1.get_permission()!=permissions) return false;
+    return true;
 }
 
 
-
-
 // Demonstrate some basic assertions.
-TEST(GraderTest, CorrectGrade) {
-
+TEST(UserTests, current) {
+    EXPECT_EQ(creating_user(), true);
 
 }
